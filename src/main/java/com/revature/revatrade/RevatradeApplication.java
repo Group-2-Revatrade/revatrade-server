231fb512344528a0a4176a71c4b381839ceba0a2
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-03 11:36:33 -0600

    initial commit

---
+++ src/main/java/com/revature/revatrade/RevatradeApplication.java
@@ -0,0 +1,13 @@
+package com.revature.revatrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevatradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevatradeApplication.class, args);
+	}
+
+}
