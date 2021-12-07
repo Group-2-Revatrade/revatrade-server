33bce22efa1c340a3a5683f93f9345544ca7a230
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-12-07 11:10:44 -0500

    feat: Added new UserControllerTest testSaveUser.

---
+++ src/test/java/com/revature/revatrade/controller/UserControllerTest.java
@@ -0,0 +1,76 @@
+package com.revature.revatrade.controller;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.revature.revatrade.model.User;
+import com.revature.revatrade.service.UserService;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+	@Mock
+	private UserService userService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private UserControllerTest userController;
+
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
+
+	@Test
+	public void testSaveUser() throws Exception {
+		User newUser = new User(0,"newUserUserName","123abcdef","abcd123@revatrade.com", "customer");
+		when(userService.saveUser(Mockito.any(User.class))).thenReturn(newUser);
+		this.mockMvc.perform(post("/api/signup")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(asJsonString(newUser)));
+		verify(userService,times(1)).saveUser(newUser);
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
