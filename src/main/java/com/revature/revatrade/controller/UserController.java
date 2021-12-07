2c7c73ae4802f94542648b69dd9815796d3bacb8
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-12-07 08:44:30 -0500

    feat: Added new UserController method saveUser.

--- src/main/java/com/revature/revatrade/controller/UserController.java
+++ src/main/java/com/revature/revatrade/controller/UserController.java
@@ -1,20 +1,32 @@
 package com.revature.revatrade.controller;
 
+import com.revature.revatrade.model.User;
 import com.revature.revatrade.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("userController")
 @RequestMapping(value = "api")
 @CrossOrigin(value = "http://localhost:4200", allowCredentials = "true")
 public class UserController {
-    UserService userService;
+	UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 
+	@PostMapping(path="/signup", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public User saveUser(@RequestBody User user) {
+		try {
+			return this.userService.saveUser(user);
+		} catch(org.springframework.dao.DataIntegrityViolationException e) {
+			return null;
+		}
+	}
 }
--- src/main/java/com/revature/revatrade/service/UserService.java
+++ src/main/java/com/revature/revatrade/service/UserService.java
@@ -1,16 +1,20 @@
 package com.revature.revatrade.service;
 
+import com.revature.revatrade.model.User;
 import com.revature.revatrade.repository.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
 public class UserService {
-    UserDao userDao;
+	UserDao userDao;
 
-    @Autowired
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
-    }
+	@Autowired
+	public UserService(UserDao userDao) {
+			this.userDao = userDao;
+	}
 
+	public User saveUser(User user) {
+		return this.userDao.saveUser(user);
+	}
 }
