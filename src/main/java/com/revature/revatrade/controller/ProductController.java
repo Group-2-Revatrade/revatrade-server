a38fc45f989ac3b7312aae0e78bc6cb3e0c72f48
jonathan.powers <jonathan.powers@revature.net>
2021-12-08 11:35:39 -0500

    Update ProductController.java

--- src/main/java/com/revature/revatrade/controller/ProductController.java
+++ src/main/java/com/revature/revatrade/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class ProductController {
 	}
 	
 	@GetMapping(path="search", produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Product>> searchFor(@RequestParam String term){
+	public ResponseEntity<List<Product>> searchFor(@PathVariable String term){
 		return new ResponseEntity<List<Product>>(this.productService.searchFor(term), HttpStatus.OK);
 	}
 }
