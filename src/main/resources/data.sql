e802238de7a4b9d9e73d9a6397411b99db639dbf
VaziDev <Nganga25@Gmail.com>
2021-12-09 08:52:54 -0800

    update to CartController and CartServices

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,1 @@
-INSERT INTO users (userId, name, username, password) values (1, 'naresh', 'nkg101', 'password');
-
-
+INSERT INTO user (userId, name, username, password) values (1, "naresh", "nkg101", "password");
