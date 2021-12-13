4a0682d19fc0d7db6a8a73a72abe9d8a5b495aae
nikDerek <87335030+nikDerek@users.noreply.github.com>
2021-12-12 18:49:18 -0600

    Update ProductController.java

--- src/main/java/com/revature/revatrade/controller/ProductController.java
+++ src/main/java/com/revature/revatrade/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.revatrade.model.Product;
@@ -38,9 +39,9 @@ public class ProductController {
 		
 	}
 	
-// 	@GetMapping(path="search", produces=MediaType.APPLICATION_JSON_VALUE)
-// 	public ResponseEntity<List<Product>> searchFor(@RequestParam(value = "term", required = true) String term){
-// 		return new ResponseEntity<List<Product>>(this.productService.searchFor(term), HttpStatus.OK);
-// 	}
+	@GetMapping(path="search", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Product>> searchFor(@RequestParam(value = "term", required = true) String term){
+		return new ResponseEntity<List<Product>>(this.productService.searchFor(term), HttpStatus.OK);
+	}
 
 }
