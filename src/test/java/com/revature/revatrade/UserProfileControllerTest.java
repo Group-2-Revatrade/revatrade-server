dc453894c48297c907496c3fc95b4314f50b7ea6
Anthony Veronda <anthony.veronda@revature.net>
2021-12-13 15:11:53 -0700

    tests all working

--- src/test/java/com/revature/revatrade/UserProfileControllerTest.java
+++ src/test/java/com/revature/revatrade/UserProfileControllerTest.java
@@ -1,58 +1,63 @@
-// package com.revature.revatrade;
+package com.revature.revatrade;
 
-// import com.revature.revatrade.model.UserProfile;
-// import com.revature.revatrade.repository.UserDao;
-// import com.revature.revatrade.repository.UserProfileDao;
-// import com.revature.revatrade.service.UserProfileService;
-// import com.revature.revatrade.service.UserService;
-// import org.checkerframework.checker.units.qual.A;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.web.client.TestRestTemplate;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
+import com.revature.revatrade.model.UserProfile;
+import com.revature.revatrade.repository.UserDao;
+import com.revature.revatrade.repository.UserProfileDao;
+import com.revature.revatrade.service.UserProfileService;
+import com.revature.revatrade.service.UserService;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
-// import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-// public class UserProfileControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserProfileControllerTest {
 
-//      public static final String API_USERS_USERNAME_PROFILE = "/api/users/{username}/profile";
+    public static final String API_USERS_USERNAME_PROFILE = "/api/users/";
 
-//      @Autowired
-//      TestRestTemplate testRestTemplate;
+     @Autowired
+     TestRestTemplate testRestTemplate;
 
-//      @Autowired
-//      UserService userService;
+     @Autowired
+     UserService userService;
 
-//      @Autowired
-//      UserProfileService profileService;
+     @Autowired
+     UserProfileService profileService;
 
-//      @Autowired
-//      UserDao userDao;
-//      @Autowired
-//      UserProfileDao profileDao;
+     @Autowired
+     UserDao userDao;
+     
+     @Autowired
+     UserProfileDao profileDao;
 
-//      @BeforeEach
-//      public void cleanup(){
-//           profileDao.deleteAll();
-//           userService.saveUser(TestUser.createValidUser());
-//      }
-
-
-//      @Test
-//      public void postUserProfile_whenUsernameExist_receiveOK(){
-//           String username = "username";
-//           UserProfile profile = TestProfile.createValidProfile();
-
-//           ResponseEntity<Object> response = testRestTemplate.postForEntity(API_USERS_USERNAME_PROFILE+"/create", profile, Object.class );
-//           assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//      }
+    @BeforeEach
+    public void cleanup(){
+         profileDao.deleteAll();
+         userService.saveUser(TestUser.createValidUser());
+     		
+    }
 
 
+     @Test
+     public void postUserProfile_whenUsernameExist_receiveOK(){
+          String username = "username";
+          UserProfile profile = TestProfile.createValidProfile();
+          ResponseEntity<Object> response = testRestTemplate.postForEntity(API_USERS_USERNAME_PROFILE +username+ "/profile/create", profile, Object.class );
+          assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+     }
 
-// //     public <T> ResponseEntity<T> getUser(String username, Class<T> responseType){
-// //          String url = API_USERS_USERNAME;
-// //          return testRestTemplate.getForEntity(url, responseType);
-// //     }
-// }
+
+
+    public <T> ResponseEntity<T> getUser(String username, Class<T> responseType){
+         String url = API_USERS_USERNAME_PROFILE;
+         return testRestTemplate.getForEntity(url, responseType);
+    }
+}
