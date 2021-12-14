342ff3b907050a51d935efaa1297b1c56fd5b301
Leonard N <nganga25@gmail.com>
2021-12-13 18:40:13 -0800

    Testing Started, update to tests

--- src/main/java/com/revature/revatrade/service/ShoppingCartService.java
+++ src/main/java/com/revature/revatrade/service/ShoppingCartService.java
@@ -3,8 +3,13 @@ package com.revature.revatrade.service;
 import com.revature.revatrade.model.OrderDetails;
 import com.revature.revatrade.repository.ShoppingCartDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.DirectoryStream;
+import java.util.Collections;
 import java.util.List;
 
 @Service("cartService")
@@ -24,11 +29,17 @@ public class ShoppingCartService {
         return this.cartDao.findAll();
     } //verified
 
-    public void deleteItem(OrderDetails orderDetails) {
-        this.cartDao.deleteById(orderDetails.getOrderDetailsId());
+    public void deleteItem(Integer order_id) {
+        this.cartDao.deleteById(order_id);
     }
 
     public void updateCart(OrderDetails orderDetails) { //verify if saving all or just single item
         this.cartDao.save(orderDetails);
+    }
+
+    public List<OrderDetails> findAllById(Integer userId) {
+
+        return this.cartDao.findOrdersByID(userId).stream().toList();
+
     }
 }
